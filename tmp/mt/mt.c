67b1712922699d4b211a9a02021aeb0d0734d710
silas anderson <64657399+silasanderson@users.noreply.github.com>
2021-02-27 09:50:57 -0800

    Add files via upload

--- tmp/mt/mt.c
+++ tmp/mt/mt.c
@@ -1,1 +1,39 @@
+#include <stdio.h> 
+#include <time.h>       
+#include <math.h>
+ 
+int main( int argc, char **argv)
+{
 
+    time_t rawtime = time(NULL);
+    
+    struct tm *ptm = localtime(&rawtime);
+    
+	float ms =(ptm->tm_min*60 + ptm->tm_hour*3600 + ptm->tm_sec)/.864;
+	float sec =fmod(ms,100);
+	float min =fmod(((ms-sec)/100),100);
+	float hour =(ms-sec-min*100)/10000;
+
+	while ((++argv)[0]) {
+		if (argv[0][0] == '-' ) {
+
+			switch (argv[0][1])  {
+				case 'm':
+					printf("%.0f:%.0f\n", hour, min);
+					break;
+				case 's':
+					printf("%f\n", ms);
+					break;
+				default:
+					printf("mt: Unknown option argument: %s\n", argv[1]);
+			}
+		}
+	}
+
+	if ( argc == 1) {
+		printf("%.0f:%.0f:%.0f\n", hour, min, sec);
+	}
+
+	/* printf("%.0f:%.0f:%.0f\n", hour, min, sec); */
+    return 0;
+}
